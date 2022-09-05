ecd2cf3915600f71994c5c7a3072e0e7708fb4e5
Liudmila Lebedeva <cherubina2014@gmail.com>
2022-09-05 21:41:13 +0200

    first commit

---
+++ budget.js
@@ -0,0 +1,27 @@
+// SELECT ELEMENTS
+
+const balanceEl = document.querySelector(".balance .value");
+const incomeTolalEl = document.querySelector(".income-total");
+const outcomeTolalEl = document.querySelector(".outcome-total");
+const incomeEl = document.querySelector("#income");
+const expenseEl = document.querySelector("#expense");
+const AllEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income .list");
+const expenseList = document.querySelector("#outcome .list");
+const AllList = document.querySelector("#all .list");
+
+// SELECT BUTTONS
+
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// SELECT INPUTS 
+//expenses
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.querySelector(".expense-title-input");
+const expenseAmount = document.querySelector(".expense-amount-input");
+//income
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.querySelector(".income-title-input");
+const incomeAmount = document.querySelector(".income-amount-input");
